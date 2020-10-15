f4f8f81a6bf0a0f99e86007d006ed5588c95be33
Matt Talda <matthew.talda@appliedis.com>
2020-10-15 13:19:44 -0400

    updated IP option

--- ApiServer.js
+++ ApiServer.js
@@ -1,12 +1,11 @@
 const Hapi = require('@hapi/hapi');
 const router = require('./router');
-const env = process.env;
 
 const init = async() => {
 
     const server = new Hapi.Server({
-        port: env.Port || 8080,
-        host: env.IP || 'localhost'
+        port: process.env.Port,
+        host: process.env.IP
     });
 
     router.init(server);
